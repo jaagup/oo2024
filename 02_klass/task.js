cd29e280dfb2a33bca5ace30d2300b1f8e190b43
ruudiac <133976166+ruudiac@users.noreply.github.com>
2024-02-21 20:29:10 +0200

    Add files via upload
    2 kodutöö, klass

---
+++ 02_klass/task.js
@@ -0,0 +1,28 @@
+class Task {
+  constructor(name, description, deadline, priority) {
+      this.name = name;
+      this.description = description;
+      this.deadline = deadline;
+      this.priority = priority;
+  }
+
+  displayTaskInfo() {
+      console.log(`Task: ${this.name}`);
+      console.log(`Description: ${this.description}`);
+      console.log(`Deadline: ${this.deadline}`);
+      console.log(`Priority: ${this.priority}`);
+  }
+}
+
+// Esimene ülesanne
+const task1 = new Task("Projekti aruande koostamine", "Aruande kirjutamine ja vormistamine", new Date("2024-02-28"), 2);
+
+// Teine ülesanne
+const task2 = new Task("Kodu koristamine", "Tolmuimejaga koristamine ja pesemine", new Date("2024-03-15"), 1);
+
+// Näita ülesande infot
+console.log("Esimene ülesanne:");
+task1.displayTaskInfo();
+
+console.log("\nTeine ülesanne:");
+task2.displayTaskInfo();
